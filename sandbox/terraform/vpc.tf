afa1ed69b58514b85648b600a6331ba4cc200405
Victor Batista <victorbatistax@gmail.com>
2022-10-15 17:03:50 -0300

    Alter vpc

--- sandbox/terraform/vpc.tf
+++ sandbox/terraform/vpc.tf
@@ -6,6 +6,7 @@ module "vpc" {
   cidr = local.vpc_cidr
 
   azs  = local.azs
+  public_subnets  = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 4, k)]
   private_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 4, k + 10)]
 
   enable_nat_gateway   = true
